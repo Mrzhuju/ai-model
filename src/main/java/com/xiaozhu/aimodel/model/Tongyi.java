9c7e01da0e0a0372b6d798ea45e184f09ba2b825
zhujusen <zhujusen@meizu.com>
2024-02-06 16:59:16 +0800

    [#0][#功能新增]通义万相sdk接入

---
+++ src/main/java/com/xiaozhu/aimodel/model/Tongyi.java
@@ -0,0 +1,54 @@
+package com.xiaozhu.aimodel.model;
+
+// Copyright (c) Alibaba, Inc. and its affiliates.
+import com.alibaba.dashscope.aigc.imagesynthesis.ImageSynthesis;
+import com.alibaba.dashscope.aigc.imagesynthesis.ImageSynthesisListResult;
+import com.alibaba.dashscope.aigc.imagesynthesis.ImageSynthesisParam;
+import com.alibaba.dashscope.aigc.imagesynthesis.ImageSynthesisResult;
+import com.alibaba.dashscope.task.AsyncTaskListParam;
+import com.alibaba.dashscope.exception.ApiException;
+import com.alibaba.dashscope.exception.NoApiKeyException;
+import com.alibaba.dashscope.utils.JsonUtils;
+
+public class Tongyi {
+    public static void basicCall() throws ApiException, NoApiKeyException {
+        ImageSynthesis is = new ImageSynthesis();
+        ImageSynthesisParam param =
+                ImageSynthesisParam.builder()
+                        .model(ImageSynthesis.Models.WANX_V1)
+                        .n(4)
+                        .size("1024*1024")
+                        .prompt("女主角一袭白色长裙，长发飘飘，手里拿着一束白玫瑰，场景美得就像从画里走出来一样。")
+                        .build();
+
+        ImageSynthesisResult result = is.call(param);
+        System.out.println(result);
+        System.out.println(JsonUtils.toJson(result));
+    }
+
+    public static void listTask() throws ApiException, NoApiKeyException {
+        ImageSynthesis is = new ImageSynthesis();
+        AsyncTaskListParam param = AsyncTaskListParam.builder().build();
+        ImageSynthesisListResult result = is.list(param);
+        System.out.println(result);
+    }
+
+    public void fetchTask() throws ApiException, NoApiKeyException {
+        String taskId = "your task id";
+        ImageSynthesis is = new ImageSynthesis();
+        // If set DASHSCOPE_API_KEY environment variable, apiKey can null.
+        ImageSynthesisResult result = is.fetch(taskId, null);
+        System.out.println(result.getOutput());
+        System.out.println(result.getUsage());
+    }
+
+    public static void main(String[] args) {
+        try {
+            basicCall();
+            //listTask();
+        } catch (ApiException | NoApiKeyException e) {
+            System.out.println(e.getMessage());
+        }
+        System.exit(0);
+    }
+}
